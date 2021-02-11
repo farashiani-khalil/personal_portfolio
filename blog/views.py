4df7543f9ed80cb652808608512ae35772c6d6c2
farashiani-khalil <farashiani.khalil@gmail.com>
2021-02-11 07:49:05 -0500

    working with database lvl2

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,7 @@
 from django.shortcuts import render
+
+from .models import Blogs
+
+def show_blogs(request):
+    blogs = Blogs.objects.order_by('-date')[:2]
+    return render(request,'blog/show_blogs.html',{ 'blogs' : blogs})
